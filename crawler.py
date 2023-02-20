4e73a2115acf99aba29dc51cca884714246aac59
xlv8 <110122373+xlv8@users.noreply.github.com>
2023-02-20 12:14:02 +0000

    bump version to 2.1.1

--- crawler.py
+++ crawler.py
@@ -8,3 +8,10 @@ BASE_URL = 'https://stackoverflow.com/questions/tagged/c'
 SORT = '?sort=votes'
 PAGE = '&page='
 PAGE_SIZE_URL = '&pageSize='
+
+PAGE_SIZE = 15
+NUM_ANSWERS = 3
+
+headers = {
+	'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+}
